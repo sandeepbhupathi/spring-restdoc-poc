85e5b28e286082265369afaf3692f54d17134f0d
sandeepbupathi08 <sandeepbupathi08@gmail.com>
2016-10-24 07:55:05 -0400

    initial commit

---
+++ src/main/java/com/sandeep/RestdocSamplenewApplication.java
@@ -0,0 +1,12 @@
+package com.sandeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocSamplenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdocSamplenewApplication.class, args);
+	}
+}
